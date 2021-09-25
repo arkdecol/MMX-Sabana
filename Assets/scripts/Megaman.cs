b3dbea6bf1fd3ea1ac749cc21af998c7b4df3113
Jose Striedinger <jstriedinger2090@gmail.com>
2021-09-25 15:14:12 -0500

    Clase 20 Sept

--- Assets/scripts/Megaman.cs
+++ Assets/scripts/Megaman.cs
@@ -5,26 +5,58 @@ using UnityEngine;
 public class Megaman : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] BoxCollider2D pies;
+    [SerializeField] float jumpSpeed;
+
     Animator myAnimator;
+    Rigidbody2D myBody;
+
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Mover();
+        Saltar();
+
+    }
+
+    void Mover()
+    {
         float mov = Input.GetAxis("Horizontal");
-        if(mov != 0)
+        if (mov != 0)
         {
+            transform.localScale = new Vector2(Mathf.Sign(mov), 1);
             myAnimator.SetBool("running", true);
+            transform.Translate(new Vector2(mov * speed * Time.deltaTime, 0));
 
         }
         else
             myAnimator.SetBool("running", false);
-
-        transform.Translate(new Vector2(mov * speed * Time.deltaTime, 0));
-
     }
+
+    void Saltar()
+    {
+        bool isGrounded = pies.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        if (isGrounded)
+        {
+            myAnimator.SetBool("falling", false);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                myAnimator.SetTrigger("takeof");
+                myBody.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+
+            }
+        }
+        else
+            myAnimator.SetBool("falling", true);
+        
+    }
+
 }
